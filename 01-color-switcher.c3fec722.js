13c9acec03dbe566ce08f8734aab561fa6133ba7
TryHungury <105644270+TryHungury@users.noreply.github.com>
2022-11-04 13:34:06 +0000

    Deploying to gh-pages from @ TryHungury/goit-js-hw-09@e4c3c6dda8f90aaeef11e6c864a690d014efbd6d 🚀

---
+++ 01-color-switcher.c3fec722.js
@@ -0,0 +1,2 @@
+const t={bodyBcg:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=null;t.startBtn.addEventListener("click",(r=>{r.isTrusted&&(e=setInterval((()=>{t.bodyBcg.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.c3fec722.js.map
